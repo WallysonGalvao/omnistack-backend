7b7140db9e30a1d5a1de06ef9abb88b40d1d1b04
Wallyson Galvão <wallyson.galvao@gmail.com>
2019-04-16 19:53:38 -0300

    Adicionado cors ao projeto

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const server = require('http').Server(app);
@@ -21,6 +22,8 @@ app.use(( req, res, next ) => {
     req.io = io;
     return next();
 });
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
