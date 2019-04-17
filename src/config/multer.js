e2c0406f8742f682f17c6121e4cc6a881c4337df
Wallyson Galvão <wallyson.galvao@gmail.com>
2019-04-16 22:28:37 -0300

    Correções no file

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,21 +1,21 @@
-const multer = require('multer');
-const path = require('path');
-const crypto = require('crypto');
+const multer = require('multer')
+const path = require('path')
+const crypto = require('crypto')
 
-module.export = {
-    dest: path.resolve(__dirname, '..', '..', 'temp'),
+module.exports = {
+    dest: path.resolve(__dirname, '..', '..', 'tmp'),
     storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, path.resolve(__dirname, '..', '..', 'temp'));
+        destination: (req, file, callback) => {
+            callback(null, path.resolve(__dirname, '..', '..', 'tmp'))
         },
-        filename: () => {
+        filename: (req, file, callback) => {
             crypto.randomBytes(16, (err, hash) => {
-                if (err) cb(err);
+                if (err) callback(err)
 
-                file.key = `${hash.toString('hex')}-${file.originalname}`;
+                file.key = `${hash.toString('hex')}-${file.originalname}`
 
-                cb(null, file.key);
+                callback(null, file.key)
             })
         }
     })
-};
+}
--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,19 +1,23 @@
-const Box = require('../models/Box');
-const File = require('../models/File');
+const File = require('../models/File')
+const Box = require('../models/Box')
 
 class FileController {
     async store(req, res) {
-        const box = await Box.findById(req.params.id);
+        const box = await Box.findById(req.params.id)
+
         const file = await File.create({
             title: req.file.originalname,
             path: req.file.key
-        });
+        })
 
-        box.files.push(file);
-        await box.save();
-        req.io.sockets.in(box._id).emit('file', file);
-        return res.json(file);
+        box.files.push(file)
+
+        await box.save()
+
+        req.io.sockets.in(box._id).emit('file', file)
+
+        return res.json(file)
     }
 }
 
-module.exports = new FileController();
+module.exports = new FileController()
